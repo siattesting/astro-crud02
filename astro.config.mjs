d154a5358d590478f5c30913a343ef076c9aea0e
siat.testing <siat.testing@gmail.com>
2023-10-06 22:08:44 +0000

    Auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from '@astrojs/vercel/serverless';
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   // see PocketBase Javascript sdk
   adapter: vercel(),
+  integrations: [solidJs()]
 });
