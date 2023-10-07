cae3ecdeb88cf28aa10e5bb49c2cc0768b593dea
siat.testing <siat.testing@gmail.com>
2023-10-07 19:09:46 +0000

    Minor updates

--- src/service/TransactionsService.ts
+++ src/service/TransactionsService.ts
@@ -10,30 +10,6 @@ export interface Transaction {
   created: number;
 }
 
-export interface Chat {
-  id: string;
-  first: string;
-  firstName: string;
-  second: string;
-  secondName: string;
-  messages: Message[];
-}
-
-export async function createChat(
-  first: string,
-  firstName: string,
-  second: string,
-  secondName: string
-): Promise<Chat> {
-  return await POCKET.collection('chats').create<Chat>({
-    first,
-    firstName,
-    second,
-    secondName,
-    messages: [],
-  });
-}
-
 export async function createTransaction() {
   return await POCKET.collection('transactions').create({
     author: string,
@@ -61,26 +37,4 @@ export async function getTransaction(id: string) {
 
 export async function getAllTransactions() {
   return await POCKET.collection('transactions').getFullList({});
-}
-
-export async function updateChat(id: string, messages: Message[]) {
-  return await POCKET.collection('chats').update(id, {
-    messages,
-  });
-}
-
-export async function getUserChats(link: string): Promise<Chat[]> {
-  const result = await POCKET.collection('chats').getList<Chat>(1, 50, {
-    filter: `first='${link}' || second='${link}'`,
-  });
-  return result.items;
-}
-
-export async function getChat(id: string) {
-  return await POCKET.collection('chats').getOne(id);
-}
-
-export function getChatName(chat: Chat, link: string) {
-  const name = chat.first === link ? chat.secondName : chat.firstName;
-  return name[0].toUpperCase();
 }
